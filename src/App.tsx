41297f67fbf14b4fb341fff1ec636fde98d9b937
sanjaylikeminds <sanjay.kumar@likeminds.community>
2024-02-14 10:32:59 +0530

    Adding routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,50 @@
-// import { useContext } from "react";
-import { Theme } from "./Themes/ThemeClass";
-import Header from "./components/Header/Header";
-import LMClientOverlayProvider from "./components/LMChatProvider/LMClientOverlayProvider";
-import LMChatView from "./components/LMChatView/LMChatView";
-import Input from "./components/LMInput/Input";
-import MessageList from "./components/LMMessageList/MessageList";
-// import MessageContext from "./context/MessageContext";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import LMChatClient from "@likeminds.community/chat-js-beta";
 
-import "./App.css";
-import LMChannelList from "./components/LMChannelList/LMChannelList";
-import { LMClient } from "./types/DataLayerExportsTypes";
+import { Theme } from "./Themes/ThemeClass";
+import LMClientOverlayProvider from "./components/LMChatProvider/LMClientOverlayProvider";
+import LMChatView from "./components/LMChatView/LMChatView";
 
-function App() {
+import "./App.css";
+import { LMClient } from "./types/DataLayerExportsTypes";
+import Error from "./shared/Error";
+import Channel from "./components/channel/Channel";
+
+const LMAppLayout = () => {
   const myClient: LMClient = LMChatClient.setApiKey(
-    "8fa4304d-a5b6-4f10-baeb-a80650a480a4"
+    "8fa4304d-a5b6-4f10-baeb-a80650a480a4",
   )
     .setPlatformCode("rt")
     .setVersionCode(35)
     .build();
   return (
-    <>
-      <LMClientOverlayProvider client={myClient} theme={new Theme().theme}>
-        <LMChatView>
-          <div className="d-flex">
-            <div className="lm-left-panel">
-              <LMChannelList />
-            </div>
-            <div className="lm-right-panel d-flex flex-direction">
-              <Header />
-              <MessageList />
-              <Input />
-            </div>
-          </div>
-        </LMChatView>
-      </LMClientOverlayProvider>
-    </>
+    <LMClientOverlayProvider client={myClient} theme={new Theme().theme}>
+      <LMChatView>
+        <Outlet />
+      </LMChatView>
+    </LMClientOverlayProvider>
   );
-}
-// function CustomMessage() {
-//   const { message } = useContext(MessageContext);
-//   return (
-//     <div
-//       style={{
-//         color: "green",
-//         background: "#f0f0f0",
-//         padding: "16px",
-//         borderRadius: "8px",
-//         margin: "30px",
-//         width: "250px",
-//       }}
-//     >
-//       {message?.toString()}
-//     </div>
-//   );
-// }
+};
 
-// function CustomMessageWithClassStyling() {
-//   const { message } = useContext(MessageContext);
-//   return <div className="customClass">{message?.toString()}</div>;
-// }
-export default App;
+// Routing
+const appRoute = createBrowserRouter([
+  {
+    path: "/",
+    element: <LMAppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Channel />,
+      },
+    ],
+    errorElement: <Error />,
+  },
+]);
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement,
+);
+root.render(<RouterProvider router={appRoute} />);
+
+export default LMAppLayout;
