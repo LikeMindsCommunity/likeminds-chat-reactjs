4747bfc5034ff338acb33273d8b725f46c1c3384
sanjaylikeminds <sanjay.kumar@likeminds.community>
2024-06-28 17:18:15 +0530

    ui changes

---
+++ src/index.tsx
@@ -0,0 +1,49 @@
+import LMChatChatroom from "./components/channel/LMChatChatroom";
+import Channel from "./components/channel/Channel";
+import ScrollContainer from "./components/DualSidePagination/ScrollContainer";
+import Header from "./components/Header/Header";
+import LMChannelList from "./components/LMChannelList/LMChannelList";
+import LMClientOverlayProvider from "./components/LMChatProvider/LMClientOverlayProvider";
+import AttachmentsSelector from "./components/LMInput/AttachmentsSelector";
+import MediaCarousel from "./components/LMInput/Carousel";
+import Emojis from "./components/LMInput/Emojis";
+import GiphySearch from "./components/LMInput/GiphySearch";
+import GiSelector from "./components/LMInput/GiSelector";
+import Input from "./components/LMInput/Input";
+import LMChatTextArea from "./components/LMInput/LMChatTextArea";
+import { ReactGiffySearchComponent } from "./components/LMInput/ReactGiffySearchComponent";
+import Loader from "./components/LMLoader/Loader";
+import LMMessageBubble from "./components/LMMessage/LMMessageBubble";
+import LMNormalMessage from "./components/LMMessage/LMNormalMessage";
+import Message from "./components/LMMessage/Message";
+import MessageOptions from "./components/LMMessage/MessageOptions";
+import MessageReactionHolder from "./components/LMMessage/MessageReactionHolder";
+import Reactions from "./components/LMMessage/Reactions";
+import ReportTagComponent from "./components/LMMessage/ReportTagComponent";
+import ReportTagsDialog from "./components/LMMessage/ReportTags";
+
+export {
+  LMChatChatroom,
+  Channel,
+  ScrollContainer,
+  Header,
+  LMChannelList,
+  LMClientOverlayProvider,
+  AttachmentsSelector,
+  MediaCarousel,
+  Emojis,
+  GiphySearch,
+  GiSelector,
+  Input,
+  LMChatTextArea,
+  ReactGiffySearchComponent,
+  Loader,
+  LMMessageBubble,
+  LMNormalMessage,
+  Message,
+  MessageOptions,
+  MessageReactionHolder,
+  Reactions,
+  ReportTagComponent,
+  ReportTagsDialog,
+};
