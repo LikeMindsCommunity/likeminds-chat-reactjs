023e67576b3a40b3472b7b5692e471d5d5855259
Sanjay <75876298+sanjaylikeminds@users.noreply.github.com>
2024-07-10 15:51:49 +0530

    Feature/lm 11148 search (#38)
    * chatroom search ui changes
    
    * search in chatroom ui changes
    
    * ui changes
    
    * ui changes
    
    * PR template added
    
    * ui issues fixed
    
    * new changes
    
    * search ui added
    
    * ui changes
    
    * ui bugs fixed
    
    * chatroom ui issues fixed
    
    * notification issues is fixed
    
    * notification issues fixed

--- core/src/hooks/useUserProvider.ts
+++ core/src/hooks/useUserProvider.ts
@@ -2,6 +2,10 @@ import { useEffect, useState } from "react";
 import { LMClient } from "../types/DataLayerExportsTypes";
 import Member from "../types/models/member";
 import { UserDetails } from "../context/GlobalClientProviderContext";
+import { getCurrentBrowserFingerPrint } from "@rajesh896/broprint.js";
+import { onMessage } from "firebase/messaging";
+import { generateToken, messaging } from "../notifications/firebase";
+import toast, { Toaster } from "react-hot-toast";
 
 interface UserProviderInterface {
   lmChatUser: null | Member;
@@ -20,6 +24,7 @@ export default function useUserProvider(
     useState<unknown>(null);
   const [lmChatUserCurrentCommunity, setLmChatUserCurrentCommunity] =
     useState<unknown>(null);
+
   useEffect(() => {
     console.log(lmChatclient);
     if (!lmChatclient) {
@@ -52,10 +57,37 @@ export default function useUserProvider(
     userDetails.username,
     userDetails.uuid,
   ]);
+
+  const [currentBrowserId, setCurrentBrowserId] = useState("");
+
+  const device = {
+    token:
+      "cAEsVZ0VuGIQEIobeSVCFz:APA91bGoweEW1ddBkGHzoH_DJ6dKwxBe-IqkG7zqTPgShAxHNGLXrLuAwikFo4tDxmcufll7_B7EIidxQ2uRz87dexSJhvKvOVK6_ada87yGFE4vA64FuqgAL5X9hH8U2cS1QQj_GQQM",
+    xDeviceId: currentBrowserId,
+    xPlatformCode: "rt",
+  };
+
+  useEffect(() => {
+    if (!lmChatUser) return;
+
+    getCurrentBrowserFingerPrint().then((fingerprint) => {
+      setCurrentBrowserId(fingerprint);
+    });
+
+    const registerDevice = client?.registerDevice(device);
+    console.log(registerDevice);
+    generateToken();
+    onMessage(messaging, (payload) => {
+      console.log("Message received. ", payload);
+      toast(payload?.notification?.body);
+    });
+  }, [lmChatUser]);
+
   function logoutUser() {
     setLmChatUser(null);
     setLmChatUserMemberState(null);
   }
+
   return {
     lmChatUser,
     lmChatUserMemberState,
