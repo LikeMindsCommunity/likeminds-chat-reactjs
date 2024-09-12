4bec0188421590e2e023749c5a7a31997642aaab
sanjaylikeminds <sanjay.kumar@likeminds.community>
2024-09-13 01:28:35 +0530

    package version updated and some ui changes

--- core/src/getClient.ts
+++ core/src/getClient.ts
@@ -1,25 +1,12 @@
-import LMChatClient from "@likeminds.community/chat-js-beta";
+import LMChatClient from "@likeminds.community/chat-js";
 
-export function initiateLMClient() {
+export function initiateLMClient(excludedConversationStates?: number[]) {
   const VERSION_CODE = 40;
   const PLATFORM_CODE = "rt";
+  const EXCLUDED_CONVERSATION_STATES = excludedConversationStates;
   const lmChatClient = LMChatClient.setPlatformCode(PLATFORM_CODE)
     .setVersionCode(VERSION_CODE)
-    .setExcludedConversationStates([0, 1, 2, 3, 7, 8, 9, 10, 11, 12])
+    .setExcludedConversationStates(EXCLUDED_CONVERSATION_STATES)
     .build();
   return lmChatClient;
 }
-
-// ConversationState Enum
-// export enum ConversationState {
-//     NORMAL = 0, // Normal Message
-//     FIRST_CONVERSATION = 1, // Chatroom First Message
-//     MEMBER_JOINED_OPEN_CHATROOM = 2, // Member joins Open Chatroom
-//     MEMBER_LEFT_OPEN_CHATROOM = 3, // Member leaves Open Chatroom
-//     MEMBER_ADDED_TO_CHATROOM = 7, // Member Added in Chatroom
-//     MEMBER_LEFT_SECRET_CHATROOM = 8, // Member leaves Secret Chatroom
-//     MEMBER_REMOVED_FROM_CHATROOM = 9, // Member is removed from a Chatroom
-//     POLL = 10, // Poll Message
-//     ALL_MEMBERS_ADDED = 11, // All members are added in a chatroom
-//     TOPIC_CHANGED = 12, // Chatroom Topic Changed
-// }
--- core/vite.config.ts
+++ core/vite.config.ts
@@ -1,56 +1,56 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  define: {
-    // By default, Vite doesn't include shims for NodeJS/
-    // necessary for segment analytics lib to work
-    global: {},
-  },
-});
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   define: {
+//     // By default, Vite doesn't include shims for NodeJS/
+//     // necessary for segment analytics lib to work
+//     global: {},
+//   },
+// });
 
 /* eslint-disable no-undef */
 // vite.config.js
-// import { resolve } from "path";
-// import { defineConfig } from "vite";
-// // import scss from 'rollup-plugin-scss'
-// import react from "@vitejs/plugin-react";
-// import dts from "vite-plugin-dts";
-// import { libInjectCss } from "vite-plugin-lib-inject-css";
-// export default defineConfig({
-//   plugins: [react(), libInjectCss(), dts({ include: ["src"] })],
-//   build: {
-//     cssCodeSplit: true,
-//     copyPublicDir: false,
-//     lib: {
-//       // Could also be a dictionary or array of multiple entry points
-//       entry: resolve(__dirname, "src/index.ts"),
-//       name: "lm-chat-lib",
-//       // the proper extensions will be added
-//       fileName: "index",
-//     },
-//     rollupOptions: {
-//       // make sure to externalize deps that shouldn't be bundled
-//       // into your library
-//       external: [
-//         "react",
-//         "react-dom",
-//         "vite",
-//         "react/jsx-runtime",
-//         "react-router-dom",
-//       ],
-//       output: {
-//         // Provide global variables to use in the UMD build
-//         // for externalized deps
-//         globals: {
-//           react: "React",
-//         },
-//       },
-//     },
-//   },
-//   optimizeDeps: {
-//     disabled: false,
-//   },
-// });
+import { resolve } from "path";
+import { defineConfig } from "vite";
+// import scss from 'rollup-plugin-scss'
+import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
+import { libInjectCss } from "vite-plugin-lib-inject-css";
+export default defineConfig({
+  plugins: [react(), libInjectCss(), dts({ include: ["src"] })],
+  build: {
+    cssCodeSplit: true,
+    copyPublicDir: false,
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "lm-chat-lib",
+      // the proper extensions will be added
+      fileName: "index",
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: [
+        "react",
+        "react-dom",
+        "vite",
+        "react/jsx-runtime",
+        "react-router-dom",
+      ],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          react: "React",
+        },
+      },
+    },
+  },
+  optimizeDeps: {
+    disabled: false,
+  },
+});
